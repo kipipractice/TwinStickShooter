569bb8e509076267bbdfaff632d7a06be442065b
Krasimir Georgiev <krasi2405@gmail.com>
2019-07-04 14:18:14 +0300

    Add main menu with play, basic settings and quit functionality. + Add choosing type of input device from settings and saving it for further usage.

---
+++ Source/TwinStickShooterTut/MainMenuHUD.h
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "MainMenuHUD.generated.h"
+
+
+enum class InputType : uint8;
+
+/**
+ * 
+ */
+UCLASS()
+class TWINSTICKSHOOTERTUT_API AMainMenuHUD : public AHUD
+{
+	GENERATED_BODY()
+	
+protected:
+	
+	UFUNCTION(BlueprintCallable)
+	void SetInputType(InputType Type);
+
+	
+
+private:
+	InputType Type;
+
+	UFUNCTION(BlueprintCallable)
+	void SaveSettings();
+};
